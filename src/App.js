6f3eb69533ebadc79ee750a2a605c3f1de8d4d1f
VinitM305 <114692166+VinitM305@users.noreply.github.com>
2023-09-16 15:10:18 +0530

    Files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import {Route,Switch} from 'react-router-dom';
+import Profile from './Profile.js';
+import Announcement from './Announcement.js';
+import Results from './Results.js';
+import ChangePassword from './ChangePassword.js';
+import AboutUs from './AboutUs.js';
+import Missing from './Missing.js';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Switch>
+        <Route exact path='/'>
+          <Navbar />
+        </Route>
+        <Route exact path='/profile'>
+          <Profile/>
+        </Route>
+        <Route exact path='/announcement'>
+          <Announcement/>
+        </Route>
+        <Route exact path='/result'>
+          <Results/>
+        </Route>
+        <Route exact path='/changepassword'>
+          <ChangePassword/>
+        </Route>
+        <Route exact path='/aboutus'>
+          <AboutUs/>
+        </Route>
+        <Route exact path='/logout'>
+          <Navbar/>
+        </Route>
+        <Route path="*">
+          <Missing />
+        </Route>
+      </Switch>
+
     </div>
   );
 }
