4dc0c8bf5d860b75335e9c4af8e2b87549ccdb5b
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-11-05 16:11:12 +0530

    update company profile

--- src/Pages/CompanyProfile.js
+++ src/Pages/CompanyProfile.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Paper } from '@mui/material';
 
 function ComanyProfile() {
 
@@ -26,86 +27,75 @@ function ComanyProfile() {
   if (loading) return (<div>Loading...</div>);
 
   return (
-    <div style={{ width: '100%', backgroundColor: '#e4eaf5' }}>
-      <div className="container" style={{ width: '100%' }}>
-        <div className="main-body" style={{ width: '100%' }}>
-          <div className="row gutters-sm">
-            <div className="col-md-4 mb-3" style={{ alignItems: 'flex-start' }}>
-              <div className="card mt-0">
+    <div
+            style={{
+                position: "relative",
+                display: "flex",
+                justifyContent: "center",
+                padding: "5vh 5vw",
+
+            }}
+        >
+            <Paper sx={{ py: 1, px: 3, width: '90vw' }} className="container1">
                 <div className="card-body">
-                  <div className="d-flex flex-column align-items-center text-center" style={{ justifyContent: 'center', alignItems: 'center' }}>
-                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width={150} />
-                    <div className="mt-3">
-                      <h4>Company Logo</h4>
+                    
+                        <div className="card-body">
+                            <div className="d-flex flex-column align-items-center text-center" style={{ justifyContent: 'center', alignItems: 'center' }}>
+                                <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width={150} />
+                                <div className="mt-3">
+                                    <h4>Company Logo</h4>
+                                </div>
+                            </div>
+                        </div>
+                    <hr/ >
+                    <div className="row">
+                        <div className="col-sm-3">
+                            <h6 className="mb-0">Company Name</h6>
+                        </div>
+                        <div className="col-sm-9 text-secondary">
+                            {company?.companyname}
+                        </div>
                     </div>
-                  </div>
+                    <hr />
+                    <div className="row">
+                        <div className="col-sm-3">
+                            <h6 className="mb-0">Type (Job/SI)</h6>
+                        </div>
+                        <div className="col-sm-9 text-secondary">
+                            {company?.company_type}
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="row">
+                        <div className="col-sm-3">
+                            <h6 className="mb-0">Location</h6>
+                        </div>
+                        <div className="col-sm-9 text-secondary">
+                            {company?.address}
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="row">
+                        <div className="col-sm-3">
+                            <h6 className="mb-0">HR Name</h6>
+                        </div>
+                        <div className="col-sm-9 text-secondary">
+                            {company?.hrname}
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="row">
+                        <div className="col-sm-3">
+                            <h6 className="mb-0">Company Type</h6>
+                        </div>
+                        <div className="col-sm-9 text-secondary">
+                            {company?.company_type}
+                        </div>
+                    </div>
+                    <hr />
                 </div>
-              </div>
-            </div>
-            <div className="col-md-8">
-              <div className="card mb-3">
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Company Name</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      {company?.companyname}
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">HR Name</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      {company?.hrname}
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Contact No</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      {company?.contact}
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Email ID</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      {company?.email}
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Website</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      {company?.website}
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Company Address</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      {company?.address}
-                    </div>
-                  </div>
-                  <hr />
-                </div>
-              </div>
-            </div>
-          </div>
+            </Paper>
         </div>
-      </div>
-    </div>
   );
 }
 
