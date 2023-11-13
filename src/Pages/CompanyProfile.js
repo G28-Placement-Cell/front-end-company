a63495a9a574ec0f8eacc08f9728d3bd9da144ec
ShivangKacha <shivangkachasbk9@gmail.com>
2023-11-13 20:27:05 +0530

    verified not-verified text added

--- src/Pages/CompanyProfile.js
+++ src/Pages/CompanyProfile.js
@@ -3,31 +3,31 @@ import { Paper } from '@mui/material';
 
 function ComanyProfile() {
 
-  const [company, setCompany] = React.useState({});
-  const [loading, setLoading] = React.useState(false);
+    const [company, setCompany] = React.useState({});
+    const [loading, setLoading] = React.useState(false);
 
-  useEffect(() => {
-    console.log(localStorage.getItem('token'));
-    fetch('http://localhost:8000/api/company/profile', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-      }
-    }).then((res) => res.json()).then((data) => {
-      console.log(data);
-      setCompany(data.comp);
-      setLoading(false);
-    }).catch((err) => {
-      console.log(err);
-      setLoading(false);
-    });
-  }, [])
+    useEffect(() => {
+        console.log(localStorage.getItem('token'));
+        fetch('http://localhost:8000/api/company/profile', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        }).then((res) => res.json()).then((data) => {
+            console.log(data);
+            setCompany(data.comp);
+            setLoading(false);
+        }).catch((err) => {
+            console.log(err);
+            setLoading(false);
+        });
+    }, [])
 
-  if (loading) return (<div>Loading...</div>);
+    if (loading) return (<div>Loading...</div>);
 
-  return (
-    <div
+    return (
+        <div
             style={{
                 position: "relative",
                 display: "flex",
@@ -38,24 +38,27 @@ function ComanyProfile() {
         >
             <Paper sx={{ py: 1, px: 3, width: '90vw' }} className="container1">
                 <div className="card-body">
-                    
-                        <div className="card-body">
-                            <div className="d-flex flex-column align-items-center text-center" style={{ justifyContent: 'center', alignItems: 'center' }}>
-                                <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width={150} />
-                                <div className="mt-3">
-                                    <h4>Company Logo</h4>
-                                </div>
+
+                    <div className="card-body">
+                        <div className="d-flex flex-column align-items-center text-center" style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width={150} />
+                            <div className="mt-3">
+                                <h4>{company?.companyname}</h4>
+                                {
+                                    company?.verified ? <p id="verify" className="text-muted font-size-sm">Company profile is APPROVED </p> : <p id="verify" className="text-muted font-size-sm">Company profile is NOT APPROVED </p>
+                                }
                             </div>
                         </div>
-                    <hr/ >
-                    <div className="row">
+                    </div>
+                    <hr />
+                    {/* <div className="row">
                         <div className="col-sm-3">
                             <h6 className="mb-0">Company Name</h6>
                         </div>
                         <div className="col-sm-9 text-secondary">
                             {company?.companyname}
                         </div>
-                    </div>
+                    </div> */}
                     <hr />
                     <div className="row">
                         <div className="col-sm-3">
@@ -105,7 +108,7 @@ function ComanyProfile() {
                 </div>
             </Paper>
         </div>
-  );
+    );
 }
 
 export default ComanyProfile;
