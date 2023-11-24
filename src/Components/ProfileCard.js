48e51dc29ce0761392853c721df5001eba794703
Vedant <65vedant@gmail.com>
2023-11-25 00:56:50 +0530

    aboutus-edited

--- src/Components/ProfileCard.js
+++ src/Components/ProfileCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
+
 const ProfileCard = ({ name, username, imageSrc, socialLinks }) => {
     return (
-        <div className="col-md-12 col-xl-4">
+        <div className="col-md-12 col-lg-6 col-xl-4 mb-4">
             <div className="card1" style={{ borderRadius: '15px' }}>
                 <div className="card-body text-center">
                     <div className="mt-3 mb-4">
-                        <img src={imageSrc} className="rounded-circle img-fluid" style={{ width: '100px' }} alt="Profile Image" />
+                        <img src={imageSrc} className="rounded-circle img-fluid" style={{ maxWidth: '100%', height: 'auto' }} alt="Profile Image" />
                     </div>
                     <h4 className="mb-2">{name}</h4>
                     <p className="text-muted mb-4">@{username}</p>
